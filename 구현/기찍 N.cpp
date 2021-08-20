10721fb6bf0d75c1c335a56013773754b5a2b29a
heemang2001 <babypooh2001@naver.com>
2021-08-20 18:33:09 +0900

    인구 이동

---
+++ 구현/기찍 N.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	for (int i = N; i >= 1; i--)
+	{
+		cout << i << '\n';
+	}
+
+	return 0;
+}
